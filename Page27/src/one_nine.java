fe77e28c8b05dabb61d042f1e0b243077ea4277e
jingxaolon <1198285059@qq.com>
2020-08-29 16:50:12 +0800

    20200829 file formatted

--- Page27/src/one_nine.java
+++ Page27/src/one_nine.java
@@ -3,8 +3,8 @@ public class one_nine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("中持=");
-		System.out.println(4.9*7.9);
+		System.out.print("中持 = ");
+		System.out.println(4.9 * 7.9);
 	}
 
 }
--- Page27/src/one_six.java
+++ Page27/src/one_six.java
@@ -3,7 +3,7 @@ public class one_six {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(1+2+3+4+5+6+7+8+9);
+		System.out.println(1 + 2 + 3 + 4 + 5 +  6+ 7 + 8 + 9);
 	}
 
 }
